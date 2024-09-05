f6c7c8a1fe90f39d6c3b1512f164b7068acfc640
lichunblacktea <110118551+lichunblacktea@users.noreply.github.com>
2024-09-05 13:38:34 +0000

    py version 1

--- script.js
+++ script.js
@@ -48,26 +48,39 @@ function myfunction(){
     });
 };
 
-      // Function to update the currency input field
-      function updateCurrencyInput() {
-        const currencySelect = document.getElementById('currencySelect');
-        const currencyInput = document.getElementsByName('currencyInput')[0];
-        currencyInput.value = currencySelect.value;
-      }
-  
-      document.addEventListener('DOMContentLoaded', (event) => {
-        const currencySelect = document.getElementById('currencySelect');
-        currencySelect.addEventListener('change', updateCurrencyInput);
-      });
+// Generic function to update input fields
+function updateInput(selectId, inputName) {
+  const selectElement = document.getElementById(selectId);
+  const inputElement = document.getElementsByName(inputName)[0];
+  inputElement.value = selectElement.value;
+}
 
-	  function updateLangInput() {
-		const langSelect = document.getElementById('languageSelect'); // Update to match the id in HTML
-		const langInput = document.getElementsByName('langInput')[0];
-		langInput.value = langSelect.value;
-	  }
-	  
-	  document.addEventListener('DOMContentLoaded', (event) => {
-		const langSelect = document.getElementById('languageSelect'); // Update to match the id in HTML
-		langSelect.addEventListener('change', updateLangInput);
-	  });
-	  
+document.addEventListener('DOMContentLoaded', (event) => {
+  // Currency select event listener
+  const currencySelect = document.getElementById('currencySelect');
+  currencySelect.addEventListener('change', () => updateInput('currencySelect', 'currencyInput'));
+
+  // Language select event listener
+  const langSelect = document.getElementById('languageSelect');
+  langSelect.addEventListener('change', () => updateInput('languageSelect', 'langInput'));
+
+  // Brand select event listener
+  const brandSelect = document.getElementById('brandSelect');
+  brandSelect.addEventListener('change', () => updateInput('brandSelect', 'brandInput'));
+
+  // Skin select event listener
+  const skinSelect = document.getElementById('skinSelect');
+  skinSelect.addEventListener('change', () => updateInput('skinSelect', 'skinInput'));
+
+  // Game category select event listener
+  const gameCategorySelect = document.getElementById('gameCategorySelect');
+  gameCategorySelect.addEventListener('change', () => updateInput('gameCategorySelect', 'gameCategoryInput'));
+
+  // Game interface select event listener
+  const gameInterfaceSelect = document.getElementById('gameInterfaceSelect');
+  gameInterfaceSelect.addEventListener('change', () => updateInput('gameInterfaceSelect', 'gameInterfaceInput'));
+
+  // Table ID select event listener
+  const tabeIDSelect = document.getElementById('tabeIDSelect');
+  tabeIDSelect.addEventListener('change', () => updateInput('tabeIDSelect', 'tabeIDInput'));
+});
